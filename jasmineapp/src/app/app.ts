72f95b04bf0b8433cdbc5d19e3e79f4ee1b454a3
RamlingN <ramnijlinge@Gmail.com>
2026-04-26 13:22:56 +0530

    heading change

--- jasmineapp/src/app/app.ts
+++ jasmineapp/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('jasmineapp');
+  protected readonly title = signal('Ramling');
 }
